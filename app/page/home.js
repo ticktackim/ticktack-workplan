4c00f98cf2686cb9aec591a4057b3b3ae4415707
Dominic Tarr <dominic.tarr@gmail.com>
2017-08-12 13:19:57 +1200

    filter the home page to create index pages

--- app/page/home.js
+++ app/page/home.js
@@ -20,6 +20,16 @@ exports.needs = nest({
   'app.html.threadCard': 'first'
 })
 
+function toRecpGroup(msg) {
+  //cannocialize
+  return Array.isArray(msg.value.content.repcs) &&
+    msg.value.content.recps.map(function (e) {
+    return (isString(e) ? e : e.link)
+  }).sort().map(function (id) {
+    return id.substring(0, 10)
+  }).join(',')
+}
+
 exports.create = (api) => {
   return nest('app.page.home', function (location) {
     // location here can expected to be: { page: 'home' }
@@ -27,37 +37,80 @@ exports.create = (api) => {
 
     var container = h('div.container', [])
 
-    function threadGroup (threads, obj, toContext, opts) {
-      // threads = a state object for all the types of threads
-      // obj = a map of keys to root ids, where key (channel | group | concatenated list of pubkeys)
-      // toContext = fn that derives the context of the group
-      // opts = { nameRecipients }
-
-      var groupEl = h('div.threads')
-      for(var k in obj) {
-        var id = obj[k]
-        var thread = get(threads, ['roots', id])
-        if(thread && thread.value) {
-          var el = api.app.html.threadCard(toContext(k, thread), thread, opts)
-          if(el) groupEl.appendChild(el)
-        }
-      }
-      return groupEl
+    function filterForThread (thread) {
+      if(thread.value.private)
+        return {private: toRecpGroup(thread)}
+      else if(thread.value.content.channel)
+        return {channel: thread.value.content.channel}
     }
 
+    function filter (rule, thread) {
+      if(!thread.value) return false
+      if(!rule) return true
+      if(rule.channel) {
+        return rule.channel == thread.value.content.channel
+      }
+      else if(rule.group)
+        return rule.channel == thread.value.content.group
+      else if(rule.private)
+        return rule.private == toRecpGroup(thread)
+      else return true
+    }
+
+    var morePlease = false
     var threadsObs = api.state.obs.threads()
+    var threadsObsDebounced = function (fn) {
+      var ts = 0, timer
+      threadsObs(function (v) {
+        if(Date.now() > ts + 1000) {
+          console.log('threads', Object.keys(v.roots).length, v.stats)
+          morePlease = false
+          ts = Date.now()
+          fn(v)
+        }
+        else {
+          clearTimeout(timer)
+          timer = setTimeout(function () {
+            ts = Date.now()
+            morePlease = false
+            fn(threadsObs.value)
+          }, 1000)
+        }
+        if(morePlease) threadsObs.more()
+      })
+
+    }
+
+    threadsObsDebounced.more = function () {
+      morePlease = true
+      threadsObs.more()
+    }
 
     var threadsHtmlObs = More(
-      threadsObs,
+      threadsObsDebounced,
       function render (threads) {
         morphdom(container,
+          //some of these containers could be removed
+          //but they are here to be compatible with the old MCSS.
           h('div.container', [
             //private section
             h('section.updates -directMessage', [
               h('div.threads', 
-                Object.keys(threads.roots).map(function (id) {
-                  return api.app.html
-                    .threadCard(null, threads.roots[id], opts)
+                Object.keys(threads.roots)
+                  .map(function (id) {
+                    return threads.roots[id]
+                  })
+                  .filter(function (thread) {
+                    return filter(location.filter, thread)
+                  })
+                  .map(function (thread) {
+                    var el = api.app.html
+                      .threadCard(null, thread, opts)
+                    if(!location.filter && el)
+                      el.onclick = function () {
+                        api.history.sync.push({page: 'home', filter: filterForThread(thread)})
+                      }
+                    return el
                 })
               )
             ]),
