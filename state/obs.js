89b30291f29ac9c66dd0755935660464050a1699
Dominic Tarr <dominic.tarr@gmail.com>
2017-08-14 08:48:48 +1200

    do not try to use api before things are connected up

--- state/obs.js
+++ state/obs.js
@@ -21,74 +21,80 @@ exports.needs = nest({
 })
 
 exports.create = function (api) {
+  var threadsObs
 
-  var initial
-  try { initial = JSON.parse(localStorage.threadsState) }
-  catch (_) { }
+  return nest('state.obs.threads',   function buildThreadObs() {
+    if(threadsObs) return threadsObs
 
-  var lastTimestamp = initial ? initial.last : Date.now()
-  var firstTimestamp = initial ? initial.last : Date.now()
+    var initial
+    try { initial = JSON.parse(localStorage.threadsState) }
+    catch (_) { }
 
-  function unbox () {
-    return pull(
-      pull.map(function (data) {
-        lastTimestamp = data.timestamp
-        if(isObject(data.value.content)) return data
-        return api.message.sync.unbox(data)
-      }),
-      pull.filter(Boolean)
+    var lastTimestamp = initial ? initial.last : Date.now()
+    var firstTimestamp = initial ? initial.last : Date.now()
+
+    function unbox () {
+      return pull(
+        pull.map(function (data) {
+          lastTimestamp = data.timestamp
+          if(isObject(data.value.content)) return data
+          return api.message.sync.unbox(data)
+        }),
+        pull.filter(Boolean)
+      )
+    }
+
+    threadsObs = PullObv(
+      threadReduce,
+      pull(
+        Next(function () {
+          return api.sbot.pull.log({reverse: true, limit: 500, lt: lastTimestamp})
+        }),
+        pull.through(function (data) {
+          lastTimestamp = data.timestamp
+        }),
+        unbox()
+      ),
+      //value recovered from localStorage
+      initial
     )
-  }
 
-  var threadsObs = PullObv(
-    threadReduce,
+    threadsObs(function (threadsState) {
+      if(threadsState.ended && threadsState.ended !== true)
+        console.error('threadObs error:', threadsState.ended)
+    })
+
+    var timer
+    //keep localStorage up to date
+    threadsObs(function (threadsState) {
+      clearTimeout(timer)
+      setTimeout(function () {
+        threadsState.last = lastTimestamp
+        localStorage.threadsState = JSON.stringify(threadsState)
+      }, 1000)
+    })
+
+    //stream live messages. this *should* work.
+    //there is no back pressure on new events
+    //only a show more on the top (currently)
+
     pull(
       Next(function () {
-        return api.sbot.pull.log({reverse: true, limit: 500, lt: lastTimestamp})
+        return api.sbot.pull.log({reverse: true, limit: 500, gte: firstTimestamp})
       }),
-      pull.through(function (data) {
-        lastTimestamp = data.timestamp
-      }),
-      unbox()
-    ),
-    //value recovered from localStorage
-    initial
-  )
-
-  threadsObs(function (threadsState) {
-    if(threadsState.ended && threadsState.ended !== true)
-      console.error('threadObs error:', threadsState.ended)
-  })
-
-  var timer
-  //keep localStorage up to date
-  threadsObs(function (threadsState) {
-    clearTimeout(timer)
-    setTimeout(function () {
-      threadsState.last = lastTimestamp
-      localStorage.threadsState = JSON.stringify(threadsState)
-    }, 1000)
-  })
-
-  //stream live messages. this *should* work.
-  //there is no back pressure on new events
-  //only a show more on the top (currently)
-
-  pull(
-    Next(function () {
-      return api.sbot.pull.log({reverse: true, limit: 500, gte: firstTimestamp})
-    }),
-    pull.drain(function (data) {
-      firstTimestamp = data.timestamp
-      threadsObs.set(threadReduce(threadsObs.value, data))
-    })
-  )
-
-  return nest('state.obs.threads',function () {
+      pull.drain(function (data) {
+        firstTimestamp = data.timestamp
+        threadsObs.set(threadReduce(threadsObs.value, data))
+      })
+    )
     return threadsObs
   })
+)
 
 }
+
+
+
 
 
 
