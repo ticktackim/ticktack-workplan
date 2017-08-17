ec2e673495eb4d062dcc4ddda74abbd628303d69
mix irving <whimful@gmail.com>
2017-08-17 15:55:26 +1200

    use lodash/get in app.page.channel

--- app/page/channel.js
+++ app/page/channel.js
@@ -2,6 +2,7 @@ const nest = require('depnest')
 const { h, computed } = require('mutant')
 const More = require('hypermore')
 const morphdom = require('morphdom')
+const get = require('lodash/get')
 
 exports.gives = nest('app.page.channel')
 
@@ -23,12 +24,12 @@ function latestUpdate(thread) {
 
 exports.create = (api) => {
   return nest('app.page.channel', function (location) {
-    // location here can expected to be: { page: 'home' }
+    const { channel } = location
     var strings = api.translations.sync.strings()
 
     var container = h('div.container', [])
 
-    var channelObs = api.state.obs.channel(location.channel)
+    var channelObs = api.state.obs.channel(channel)
 
     //disable "Show More" button when we are at the last thread.
     var disableShowMore = computed([channelObs], threads => !!threads.ended)
@@ -42,20 +43,11 @@ exports.create = (api) => {
           h('div.container', [
             //private section
             h('section.updates -directMessage', [
-              h('div.threads',
-                Object.keys(threads.roots)
-                .map(function (id) {
-                  return threads.roots[id]
-                })
-                .filter(function (thread) {
-                  return thread.value && thread.value.content.channel == location.channel
-                })
-                .sort(function (a, b) {
-                  return latestUpdate(b) - latestUpdate(a)
-                })
-                .map(function (thread) {
-                  return api.app.html.threadCard(thread)
-                })
+              h('div.threads', Object.keys(threads.roots)
+                .map(id => threads.roots[id])
+                .filter(thread => get(thread, 'value.content.channel') == channel)
+                .sort((a, b) => latestUpdate(b) - latestUpdate(a))
+                .map(thread => api.app.html.threadCard(thread))
               )
             ])
           ])
