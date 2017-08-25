2f08a973278af539f75b3b3768653ce18f35cecd
mix irving <whimful@gmail.com>
2017-08-26 10:18:42 +1200

    add quick link to your profile in header (TEMP)

--- app/html/header.js
+++ app/html/header.js
@@ -3,6 +3,8 @@ const { h, computed } = require('mutant')
 
 exports.gives = nest('app.html.header')
 
+exports.needs = nest('keys.sync.id', 'first')
+
 exports.create = (api) => {
   return nest('app.html.header', (nav) => {
     return h('Header', [
@@ -15,6 +17,7 @@ exports.create = (api) => {
         // h('h1', computed(nav.location, e => e.element.title)),
 
         h('div.tools', [
+          h('i.fa.fa-user', { 'ev-click': () => nav.push({page:'userEdit', feed: api.keys.sync.id()}) }), // TEMP
           h('i.fa.fa-address-book', { 'ev-click': () => nav.push({page:'userFind'}) }),
           h('i.fa.fa-hashtag', { 'ev-click': () => nav.push({page:'groupFind'}) }),
           h('i.fa.fa-gear', { 'ev-click': () => nav.push({page:'settings'}) })
