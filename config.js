21b4a8c3bae6a534cb6aeb6a8bb1bdfa750d2601
mix irving <whimful@gmail.com>
2017-08-26 19:12:18 +1200

    wip

--- config.js
+++ config.js
@@ -3,9 +3,9 @@ const nest = require('depnest')
 const ssbKeys = require('ssb-keys')
 const Path = require('path')
 
-const appName = process.env.ssb_appname || 'ssb' //'ticktack' TEMP: this is for the windowsSSB installer only
+const appName = process.env.ssb_appname || 'ticktack' //'ticktack' TEMP: this is for the windowsSSB installer only
 const opts = appName == 'ssb'
-  ? { "port": 43750, "blobsPort": 43751, "ws": { "port": 43751 } }
+  ? null //{ "port": 43750, "blobsPort": 43751, "ws": { "port": 43751 } }
   : require('./default-config.json')
 
 exports.gives = nest('config.sync.load')
