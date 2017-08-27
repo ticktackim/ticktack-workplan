ee8e72469e3082880b91e42e3ed64747ad35033f
mix irving <whimful@gmail.com>
2017-08-28 11:11:32 +1200

    settings page includes profile

--- app/page/settings.js
+++ app/page/settings.js
@@ -4,6 +4,10 @@ const { h } = require('mutant')
 exports.gives = nest('app.page.settings')
 
 exports.needs = nest({
+  'about.html.image': 'first',
+  'about.obs.name': 'first',
+  'history.sync.push': 'first',
+  'keys.sync.id': 'first',
   'settings.sync.get': 'first',
   'settings.sync.set': 'first',
   'settings.obs.get': 'first',
@@ -24,8 +28,32 @@ exports.create = (api) => {
       window.location.reload()
     })
 
+    const feed = api.keys.sync.id()
     const strings = api.translations.sync.strings()
     const currentLanguage = api.settings.sync.get('language')
+
+    return h('Page -settings', [
+      h('div.container', [
+        h('h1', strings.settingsPage.title),
+        h('section -profile', [
+          h('header', strings.settingsPage.section.profile),
+          h('div.profile', [
+            h('div.name', api.about.obs.name(feed)),
+            api.about.html.image(feed),
+          ]),
+          h('div.actions', [
+            h('Button', { 'ev-click': () => api.history.sync.push({page:'userEdit', feed}) }, [
+              strings.settingsPage.action.edit,
+              h('i.fa.fa-pencil')
+            ])
+          ])
+        ]),
+        h('section -language', [
+          h('header', strings.settingsPage.section.language),
+          h('div.languages', LANGUAGES.map(Language))
+        ])
+      ])
+    ])
 
     function Language (lang) {
       const selectLang = () => api.settings.sync.set({ language: lang })
@@ -40,12 +68,6 @@ exports.create = (api) => {
       )
     }
 
-    return h('Page -settings', [
-      h('div.container', [
-        h('h1', strings.settings),
-        h('div.languages', LANGUAGES.map(Language))
-      ])
-    ])
   }
 }
 
