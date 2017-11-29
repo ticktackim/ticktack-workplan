ac5e32b98d60ceac49ae4a6e8aff3fabdf642a7e
mix irving <whimful@gmail.com>
2017-11-29 20:11:01 +1300

    style blogShow consistently with blogIndex

--- app/page/blogShow.js
+++ app/page/blogShow.js
@@ -36,54 +36,32 @@ exports.create = (api) => {
 
     const { timeago, channel, markdown, compose } = api.message.html
 
-    // return api.app.html.scroller({
-    //   classList: [ 'level', '-one' ],
-    //   prepend,
-    //   stream: api.feed.pull.private,
-    //   filter: () => pull(
-    //     pull.filter(msg => msg.value.content.type === 'post'), // TODO is this the best way to protect against votes?
-    //     pull.filter(msg => msg.value.author != myKey),
-    //     pull.filter(msg => msg.value.content.recps)
-    //   ),
-    //   store: recentMsgCache,
-    //   updateTop: updateRecentMsgCache,
-    //   updateBottom: updateRecentMsgCache,
-    //   render: (msgObs) => {
-    //     const msg = resolve(msgObs)
-    //     const { author } = msg.value
-    //     if (nearby.has(author)) return
-
-    //     return Option({
-    //       notifications: Math.random() > 0.7 ? Math.floor(Math.random()*9+1) : 0, // TODO
-    //       imageEl: api.about.html.avatar(author),
-    //       label: api.about.obs.name(author),
-    //       selected: location.feed === author,
-    //       location: Object.assign({}, msg, { feed: author }) // TODO make obs?
-    //     })
-    //   }
-    // })
     return h('Page -blogShow', [
       api.app.html.context({ page: 'discover' }), // HACK to highlight discover
       h('div.content', [
-        api.app.html.blogHeader(location),
-        h('header', [
-          h('div.blog', [
-            h('h1', title),
-            timeago(blogMsg),
-            channel(blogMsg)
-          ]),
-          h('div.author', [
-            h('div.leftCol', api.about.html.avatar(author, 'medium')),
-            h('div.rightCol', [
-              h('div.name', api.about.obs.name(author)),
-              api.contact.html.follow(author)
-            ]),
-          ])
+        h('section.top', [
+          api.app.html.blogHeader(location)
         ]),
-        h('div.break', h('hr')),
-        h('section.blog', markdown(blog)),
-        comments,
-      ]),
+        h('section.content', [
+          h('header', [
+            h('div.blog', [
+              h('h1', title),
+              timeago(blogMsg),
+              channel(blogMsg)
+            ]),
+            h('div.author', [
+              h('div.leftCol', api.about.html.avatar(author, 'medium')),
+              h('div.rightCol', [
+                h('div.name', api.about.obs.name(author)),
+                api.contact.html.follow(author)
+              ]),
+            ])
+          ]),
+          h('div.break', h('hr')),
+          h('section.blog', markdown(blog)),
+          comments,
+        ]),
+      ])
     ])
   }
 }
