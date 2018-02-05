2f46470aad18da0d76890f9f0f30c34ada88472b
mix irving <whimful@gmail.com>
2018-02-06 09:01:02 +1300

    wip - multi-person PM

--- app/page/threadNew.js
+++ app/page/threadNew.js
@@ -1,10 +1,10 @@
 const nest = require('depnest')
-const { h, Struct, Value, computed } = require('mutant')
+const { h, Struct, Value, Array: MutantArray, computed, map } = require('mutant')
 
 exports.gives = nest('app.page.threadNew')
 
 exports.needs = nest({
-  'about.html.image': 'first',
+  'about.html.avatar': 'first',
   'about.obs.name': 'first',
   'app.html.sideNav': 'first',
   'app.html.thread': 'first',
@@ -22,26 +22,28 @@ exports.create = (api) => {
     const { feed, channel } = location
 
     if (feed) return threadNewFeed(location)
-    // if (channel) return threadNewChannel(location)
   }
 
   function threadNewFeed (location) {
     const strings = api.translations.sync.strings()
+    const myId = api.keys.sync.id()
 
     const { feed } = location
-    const name = api.about.obs.name(feed)
+    const name = api.about.obs.name
 
+    const searchInput = Value()
     const meta = Struct({
       type: 'post',
-      recps: [
-        api.keys.sync.id(),
-        { link: feed, name }
-      ],
+      recps: MutantArray ([
+        myId,
+        { link: feed, name: name(feed) }
+      ]),
       subject: Value()
     })
+
     const composer = api.message.html.compose(
       { meta, shrink: false },
-      (err, msg) => api.history.sync.push(err ? err : Object.assign(msg, { feed }))
+      (err, msg) => api.history.sync.push(err ? err : Object.assign(msg, { feed })) // TODO check this
     )
 
     return h('Page -threadNew', {title: strings.threadNew.pageTitle}, [
@@ -51,10 +53,11 @@ exports.create = (api) => {
           h('div.field -to', [
             h('div.label', strings.threadNew.field.to),
             h('div.recps', [
-              h('div.recp', [
-                api.about.html.image(feed),
-                h('div.name', name)
-              ])
+              map(meta.recps, Recipient),
+              h('input', {
+                'ev-input': e => searchInput.set(e.target.value),
+                placeholder: strings.optionalField
+              }),
             ])
           ]),
           h('div.field -subject', [
@@ -68,44 +71,16 @@ exports.create = (api) => {
         ])
       ])
     ])
+
+    function Recipient (r) {
+      if (r === myId) return
+
+      return h('div.recp', [
+        api.about.html.avatar(r.link, 'tiny'),
+        h('div.name', r.name)
+      ])
+    }
   }
 
-  // function threadNewChannel (location) {
-  //   const strings = api.translations.sync.strings()
-
-  //   const { channel, flash } = location
-
-  //   const meta = Struct({
-  //     type: 'post',
-  //     channel,
-  //     subject: Value()
-  //   })
-  //   const composer = api.message.html.compose(
-  //     { meta, shrink: false },
-  //     (err, msg) => api.history.sync.push(err ? err : msg)
-  //   )
-
-  //   return h('Page -threadNew', {title: strings.threadNew.pageTitle}, [
-  //     h('div.content', [
-  //       flash ? h('div.flash', flash) : '',
-  //       h('div.field -channel', [
-  //         h('div.label', strings.channel),
-  //         h('div.recps', [
-  //           h('div.recp', [
-  //             h('div.name', `#${channel}`)
-  //           ])
-  //         ])
-  //       ]),
-  //       h('div.field -subject', [
-  //         h('div.label', strings.threadNew.field.subject),
-  //         h('input', {
-  //           'ev-input': e => meta.subject.set(e.target.value),
-  //           placeholder: strings.optionalField
-  //         }),
-  //       ]),
-  //       composer
-  //     ])
-  //   ])
-  // }
 }
 
