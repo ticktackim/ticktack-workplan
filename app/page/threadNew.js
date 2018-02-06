bd8064b6ef447f00a6709b42bfe0881145a51ad8
mix <whimful@gmail.com>
2018-02-06 22:49:48 +1300

    working muiti person private messages (cap 7)

--- app/page/threadNew.js
+++ app/page/threadNew.js
@@ -1,9 +1,11 @@
 const nest = require('depnest')
-const { h, Struct, Value, Array: MutantArray, computed, map } = require('mutant')
+const { h, Struct, Value, Array: MutantArray, computed, map, resolve } = require('mutant')
+const suggestBox = require('suggest-box')
 
 exports.gives = nest('app.page.threadNew')
 
 exports.needs = nest({
+  'about.async.suggest': 'first',
   'about.html.avatar': 'first',
   'about.obs.name': 'first',
   'app.html.sideNav': 'first',
@@ -11,6 +13,7 @@ exports.needs = nest({
   'history.sync.push': 'first',
   'keys.sync.id': 'first',
   'message.html.compose': 'first',
+  'message.sync.unbox': 'first',
   'translations.sync.strings': 'first',
 })
 
@@ -29,22 +32,15 @@ exports.create = (api) => {
     const myId = api.keys.sync.id()
 
     const { feed } = location
-    const name = api.about.obs.name
 
-    const searchInput = Value()
     const meta = Struct({
       type: 'post',
       recps: MutantArray ([
         myId,
-        { link: feed, name: name(feed) }
+        { link: feed, name: resolve(api.about.obs.name(feed)) }
       ]),
       subject: Value()
     })
-
-    const composer = api.message.html.compose(
-      { meta, shrink: false },
-      (err, msg) => api.history.sync.push(err ? err : Object.assign(msg, { feed })) // TODO check this
-    )
 
     return h('Page -threadNew', {title: strings.threadNew.pageTitle}, [
       api.app.html.sideNav(location),
@@ -54,10 +50,7 @@ exports.create = (api) => {
             h('div.label', strings.threadNew.field.to),
             h('div.recps', [
               map(meta.recps, Recipient),
-              h('input', {
-                'ev-input': e => searchInput.set(e.target.value),
-                placeholder: strings.optionalField
-              }),
+              RecipientInput(meta.recps)
             ])
           ]),
           h('div.field -subject', [
@@ -67,7 +60,7 @@ exports.create = (api) => {
               placeholder: strings.optionalField
             }),
           ]),
-          composer
+          Composer(meta)
         ])
       ])
     ])
@@ -79,6 +72,75 @@ exports.create = (api) => {
         api.about.html.avatar(r.link, 'tiny'),
         h('div.name', r.name)
       ])
+    }
+
+    function RecipientInput (recps) {
+      const getRecpSuggestions = api.about.async.suggest()
+      const input = h('input', {
+        placeholder: strings.threadNew.action.addMoreRecps
+        // 'ev-input': e => searchInput.set(e.target.value),
+      })
+
+      var boxActive = false
+      addSuggest()
+
+      input.addEventListener('suggestselect', (e) => {
+        const { id, title: name } = e.detail
+        if (!recps.find(r => r === id || r.link === id))
+          recps.push({ link: id, name })
+
+        boxActive = false
+        e.target.value = ''
+        e.target.placeholder = ''
+      })
+
+      input.addEventListener('keyup', (e) => {
+        // don't pop the previous entry if still entering a name!
+        if (boxActive) {
+          // if you delete a name you were typing completely, mark box inactive
+          // so that further deletes pop names
+          if (e.target.value === '') boxActive = false
+          return
+        }
+
+        if (e.code === 'Backspace' || e.key === 'Backspace' || e.keyCode == 8) {
+          if (recps.getLength() < 3) return // can only delete down to 2 recps (sender + 1 recp)
+
+          recps.pop()
+        }
+      })
+
+      return input
+
+      function addSuggest () {
+        if (!input.parentElement) return setTimeout(addSuggest, 100)
+
+        suggestBox(input, (inputText, cb) => {
+          if (recps.getLength() >= 7) return
+          // TODO - tell the user they're only allowed 6 (or 7?!) people in a message
+
+          boxActive = true
+          const suggestions = getRecpSuggestions(inputText)
+          cb(null, suggestions)
+        }, {cls: 'PatchSuggest'})
+      }
+    }
+
+    function Composer (meta) {
+      return api.message.html.compose(
+        { meta, shrink: false },
+        (err, msg) => {
+          if (err) return api.history.sync.push(err)
+
+          const someRecipient = meta.recps.get(1)
+          if (!someRecipient) return
+
+          api.history.sync.push(Object.assign(
+            api.message.sync.unbox(msg), // for consistency of message sideNav receives D:
+            { feed: someRecipient.link }
+          ))
+        }
+      )
     }
   }
 
--- app/page/threadShow.js
+++ app/page/threadShow.js
@@ -6,6 +6,7 @@ const get = require('lodash/get')
 exports.gives = nest('app.page.threadShow')
 
 exports.needs = nest({
+  'about.html.avatar': 'first',
   'app.html.sideNav': 'first',
   'app.html.thread': 'first',
   'message.html.compose': 'first',
@@ -24,33 +25,37 @@ exports.create = (api) => {
 
     //unread state is set in here...
     const thread = api.feed.obs.thread(root)
+    const subject = get(location, 'value.content.subject')
+    const recps = get(location, 'value.content.recps')
 
     const meta = {
       type: 'post',
       root,
       branch: thread.lastId,
       channel,
-      recps: get(location, 'value.content.recps'),
+      recps
     }
     const composer = api.message.html.compose({ meta, thread, shrink: false })
 
     return h('Page -threadShow', [
       api.app.html.sideNav(location),
       h('div.content', [
-        when(thread.subject, h('h1', thread.subject)),
+        h('header', [
+          when(subject, h('h1', subject)),
+          Recipients(recps),
+        ]),
         api.app.html.thread(thread),
         composer
       ]),
     ])
   }
+
+  function Recipients (recps) {
+    if (recps && recps.length > 2)
+      return h('div.recps', recps.map(r => {
+        const recp = typeof r === 'string' ? r : r.link
+        return api.about.html.avatar(recp, 'tiny')
+      }))
+  }
 }
-
-
-
-
-
-
-
-
-
 
