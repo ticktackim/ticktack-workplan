6424f57bb37b7b4f89f528f51c04647bef49d972
mix irving <whimful@gmail.com>
2018-02-12 15:20:25 +1300

    "more subscriptions" button, minor caching, "subscribe" button

--- app/html/channelCard.js
+++ app/html/channelCard.js
@@ -1,43 +1,40 @@
 const nest = require('depnest')
 const { h, when } = require('mutant')
 
-
 exports.gives = nest('app.html.channelCard')
 
 exports.needs = nest({
-    'keys.sync.id': 'first',
-    'history.sync.push': 'first',
-    'translations.sync.strings': 'first',
-    'channel.obs.subscribed': 'first',
-    'channel.async.subscribe': 'first',
-    'channel.async.unsubscribe': 'first',
-    'channel.obs.isSubscribedTo': 'first',
+  'keys.sync.id': 'first',
+  'history.sync.push': 'first',
+  'translations.sync.strings': 'first',
+  'channel.obs.subscribed': 'first',
+  'channel.obs.isSubscribedTo': 'first',
+  'channel.async.subscribe': 'first',
+  'channel.async.unsubscribe': 'first',
+  'channel.html.subscribe': 'first',
 })
 
 exports.create = function (api) {
     
-    return nest('app.html.channelCard', (channel) => {
-        const strings = api.translations.sync.strings()
-        const myId = api.keys.sync.id()
-        const { subscribe, unsubscribe } = api.channel.async
-        const { isSubscribedTo } = api.channel.obs
-        const youSubscribe = isSubscribedTo(channel, myId)
-        
-        const goToChannel = () => {
-            api.history.sync.push({ page: 'channelShow', channel: channel })
-        }
-        
-        return h('ChannelCard', [
-            h('div.content', [
-                h('div.text', [
-                    h('h2', {'ev-click': goToChannel}, channel),
-                    when(youSubscribe,
-                        h('Button', { 'ev-click': () => unsubscribe(channel) }, strings.channelShow.action.unsubscribe),
-                        h('Button', { 'ev-click': () => subscribe(channel) }, strings.channelShow.action.subscribe)
-                    ),
-                ])
-            ])
+  return nest('app.html.channelCard', (channel) => {
+    const strings = api.translations.sync.strings()
+    const myId = api.keys.sync.id()
+    const { subscribe, unsubscribe } = api.channel.async
+    const { isSubscribedTo } = api.channel.obs
+    const youSubscribe = isSubscribedTo(channel, myId)
+    
+    const goToChannel = () => {
+      api.history.sync.push({ page: 'channelShow', channel: channel })
+    }
+    
+    return h('ChannelCard', [
+      h('div.content', [
+        h('div.text', [
+          h('h2', {'ev-click': goToChannel}, channel),
+          api.channel.html.subscribe(channel)
         ])
-    })
+      ])
+    ])
+  })
 }
 
--- channel/obs.js
+++ channel/obs.js
@@ -10,15 +10,29 @@ exports.needs = nest({
 exports.gives = nest('channel.obs.isSubscribedTo')
 
 exports.create = function (api) {
+  var subscriptions = {}
+  var myId
+
   return nest('channel.obs.isSubscribedTo', isSubscribedTo)
 
   function isSubscribedTo (channel, id) {
     if (!ref.isFeed(id)) {
-      id = api.keys.sync.id()
+      id = getMyId()
     }
         
-    const { subscribed } = api.channel.obs
-    const myChannels = subscribed(id)
-    return computed([myChannels], (v) => v.has(channel))
+    return computed(getSubscriptions(id), (v) => v.has(channel))
+  }
+
+  //cache getters
+
+  function getMyId () {
+    if (!myId) myId = api.keys.sync.id()
+    return myId
+  }
+
+  function getSubscriptions (id) {
+    if (!subscriptions[id]) subscriptions[id] = api.channel.obs.subscribed(id)
+    return subscriptions[id]
   }
 }
+
