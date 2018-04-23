0d44fc79a12ebf427d26a10693fcfce4fa2e7451
mix irving <whimful@gmail.com>
2018-04-23 14:33:08 +1200

    smooth scrolling stats

--- app/page/statsShow.js
+++ app/page/statsShow.js
@@ -1,9 +1,10 @@
 const nest = require('depnest')
-const { h, Value, Struct, Array: MutantArray, Dict, onceTrue, map, computed, dictToCollection, throttle } = require('mutant')
+const { h, resolve, when, Value, Struct, Array: MutantArray, Dict, onceTrue, map, computed, dictToCollection, throttle } = require('mutant')
 const pull = require('pull-stream')
 const marksum = require('markdown-summary')
 const Chart = require('chart.js')
 const groupBy = require('lodash/groupBy')
+const merge = require('lodash/merge')
 
 exports.gives = nest('app.page.statsShow')
 
@@ -35,10 +36,15 @@ exports.create = (api) => {
       }
     })
 
-    var rangeComments = computed([throttle(dictToCollection(store.comments), 1000), range], (comments, range) => {
+    var commentsAll = computed(throttle(dictToCollection(store.comments), 1000), (comments) => {
       return comments
         .map(c => c.value)
         .reduce((n, sofar) => [...n, ...sofar], [])
+    })
+
+    // this should perhaps be reduced to just return commentsContextCount
+    var visibleComments = computed([commentsAll, range], (comments, range) => {
+      return comments
         .filter(msg => {
           const ts = msg.value.timestamp
           return ts >= range.lower && ts <= range.upper
@@ -49,150 +55,96 @@ exports.create = (api) => {
       return likes
         .map(c => c.value)
         .reduce((n, sofar) => [...n, ...sofar], [])
-        .filter(msg => {
-          const ts = msg.value.timestamp
-          return ts >= range.lower && ts <= range.upper
-        })
+        // .filter(msg => {
+        //   const ts = msg.value.timestamp
+        //   return ts >= range.lower && ts <= range.upper
+        // })
     })
 
     onceTrue(api.sbot.obs.connection, server => {
       fetchBlogs({ server, store })
-      // fetches blogs and all associated data
 
-      // ///// test code /////
-      // var blogKey = '%3JeEg7voZF4aplk9xCEAfhFOx+zocbKhgstzvfD3G8w=.sha256'
-      // console.log('fetching comments', blogKey) // has 2 comments, 1 like
-
-      // pull(
-      //   server.blogStats.read({
-      //     gt: ['L', blogKey, null],
-      //     lt: ['L', blogKey+'~', undefined],
-      //     // gt: ['L', blogKey, null],
-      //     // lte: ['L', blogKey+'~', undefined],
-      //     // limit: 100,
-      //     keys: true,
-      //     values: true,
-      //     seqs: false,
-      //     reverse: true
-      //   }),
-      //   // pull.filter(o => o.key[1] === blogKey),
-      //   pull.log(() => console.log('DONE'))
-      // )
-      /// ///// test code /////
+      // const query = {
+      //   gt: ['C', null, range().lower],
+      //   lt: ['C', undefined, range().upper],
+      //   reverse: true,
+      //   values: true,
+      //   keys: false,
+      //   seqs: false
+      // }
+      // console.log('test query', query)
+      // pull(server.blogStats.read(query), pull.log(() => console.log('DONE')))
     })
     const canvas = h('canvas', { height: 200, width: 600, style: { height: '200px', width: '600px' } })
 
     const page = h('Page -statsShow', [
-      h('div.content', [
-        h('h1', 'Stats'),
-        h('section.totals', [
-          h('div.comments', [
-            h('div.count', computed(rangeComments, msgs => msgs.length)),
-            h('strong', 'Comments'),
-            '(30 days)'
-          ]),
-          h('div.likes', [
-            h('div.count', computed(rangeLikes, msgs => msgs.length)),
-            h('strong', 'Likes'),
-            '(30 days)'
-          ]),
-          h('div.shares', [
-          ])
-        ]),
-        h('section.graph', [
-          canvas,
-          // TODO insert actual graph
-          h('div', [
-            // h('div', [ 'Comments ', map(rangeComments, msg => [new Date(msg.value.timestamp).toDateString(), ' ']) ]),
-            // h('div', [ 'Likes ', map(rangeLikes, msg => [new Date(msg.value.timestamp).toDateString(), ' ']) ])
-          ]),
-          h('div.changeRange', [
-            h('a', { href: '#', 'ev-click': () => howFarBack.set(howFarBack() + 1) }, '< Prev 30 days'),
-            ' | ',
-            h('a', { href: '#', 'ev-click': () => howFarBack.set(howFarBack() - 1) }, 'Next 30 days >')
-          ])
-        ]),
-        h('table.blogs', [
-          h('thead', [
-            h('tr', [
-              h('th.details'),
-              h('th.comment', 'Comments'),
-              h('th.likes', 'Likes')
+      h('Scroller.content', [
+        h('div.content', [ 
+          h('h1', 'Stats'),
+          h('section.totals', [
+            h('div.comments', [
+              h('div.count', computed(visibleComments, msgs => msgs.length)),
+              h('strong', 'Comments'),
+              '(30 days)'
+            ]),
+            h('div.likes', [
+              h('div.count', computed(rangeLikes, msgs => msgs.length)),
+              h('strong', 'Likes'),
+              '(30 days)'
+            ]),
+            h('div.shares', [
             ])
           ]),
-          h('tbody', map(store.blogs, blog => h('tr.blog', { id: blog.key }, [
-            h('td.details', [
-              h('div.title', {}, getTitle(blog)),
-              h('a',
-                {
-                  href: '#',
-                  'ev-click': viewBlog(blog)
-                },
-                'View blog'
-              )
+          h('section.graph', [
+            canvas,
+            h('div.changeRange', [
+              '< ',
+              h('a', { 'ev-click': () => howFarBack.set(howFarBack() + 1) }, 'Prev 30 days'),
+              ' | ',
+              when(howFarBack,
+                h('a', { 'ev-click': () => howFarBack.set(howFarBack() - 1) }, 'Next 30 days'),
+                h('span', 'Next 30 days')
+              ),
+              ' >'
+            ])
+          ]),
+          h('table.blogs', [
+            h('thead', [
+              h('tr', [
+                h('th.details'),
+                h('th.comment', 'Comments'),
+                h('th.likes', 'Likes')
+              ])
             ]),
-            h('td.comments', computed(store.comments.get(blog.key), msgs => msgs ? msgs.length : 0)),
-            h('td.likes', computed(store.likes.get(blog.key), msgs => msgs ? msgs.length : 0))
-          // ]), { comparer: (a, b) => a === b }))
-          ])))
+            h('tbody', map(store.blogs, blog => h('tr.blog', { id: blog.key }, [
+              h('td.details', [
+                h('div.title', {}, getTitle(blog)),
+                h('a',
+                  {
+                    href: '#',
+                    'ev-click': viewBlog(blog)
+                  },
+                  'View blog'
+                )
+              ]),
+              h('td.comments', computed(store.comments.get(blog.key), msgs => msgs ? msgs.length : 0)),
+              h('td.likes', computed(store.likes.get(blog.key), msgs => msgs ? msgs.length : 0))
+            // ]), { comparer: (a, b) => a === b }))
+            ])))
+          ])
         ])
       ])
     ])
 
-    Chart.scaleService.updateScaleDefaults('linear', {
-      ticks: { min: 0 }
-    })
-    var chart = new Chart(canvas.getContext('2d'), {
-      type: 'bar',
-      data: {
-        datasets: [{
-          // label: 'My First dataset',
-          backgroundColor: 'hsla(215, 57%, 60%, 1)', // 'hsla(215, 57%, 43%, 1)',
-          borderColor: 'hsla(215, 57%, 60%, 1)',
-          // TODO set initial data as empty to make a good range
-          data: [
-          ]
-        }]
-      },
-      options: {
-        legend: {
-          display: false
-        },
-        scales: {
-          xAxes: [{
-            type: 'time',
-            distribution: 'linear',
-            time: {
-              unit: 'day',
-              min: new Date(range().lower),
-              max: new Date(range().upper)
-            },
-            bounds: 'ticks',
-            ticks: {
-              maxTicksLimit: 4
-            },
-            gridLines: {
-              display: false
-            },
-            maxBarThickness: 20
-          }],
-
-          yAxes: [{
-            ticks: {
-              suggestedMin: 0,
-              suggestedMax: 10,
-              maxTicksLimit: 5
-            }
-          }]
-        },
-        animation: {
-          duration: 300
-        }
-      }
-    })
+    // Chart.scaleService.updateScaleDefaults('linear', {
+    //   ticks: { min: 0 }
+    // })
+    var chart = new Chart(canvas.getContext('2d'), chartConfig({ range, chartData: [] }))
 
     const toDay = ts => Math.floor(ts / (24 * 60 * 60 * 1000))
-    const rangeCommentData = computed(rangeComments, msgs => {
+
+    // TODO take in context (comments/ likes / shares)
+    const chartData = computed(commentsAll, msgs => {
       const grouped = groupBy(msgs, m => toDay(m.value.timestamp))
 
       var data = Object.keys(grouped)
@@ -204,12 +156,14 @@ exports.create = (api) => {
         })
       return data
     })
-    rangeCommentData((newData) => {
-      chart.data.datasets[0].data = newData
 
-      chart.options.scales.xAxes[0].time.min = new Date(range().lower)
-      chart.options.scales.xAxes[0].time.max = new Date(range().upper)
+    chartData(() => {
+      chart = merge(chart, chartConfig({ range, chartData }))
+      chart.update()
+    })
 
+    range(() => {
+      chart = merge(chart, chartConfig({ range, chartData }))
       chart.update()
     })
 
@@ -263,4 +217,65 @@ function fetchLikes ({ server, store, blog }) {
       //   if exists - over-write or delete
     })
   )
+}
+
+function chartConfig ({ range, chartData }) {
+  const { lower, upper } = resolve(range)
+
+  const data = resolve(chartData) || []
+  const slice = data
+    .filter(d => d.t >= lower && d.t <= upper)
+    .map(d => d.y)
+    .sort((a, b) => a < b)
+  const localMax = slice[0] ? Math.max(slice[0], 10) : 10
+
+  return {
+    type: 'bar',
+    data: {
+      datasets: [{
+        // label: 'My First dataset',
+        backgroundColor: 'hsla(215, 57%, 60%, 1)', // 'hsla(215, 57%, 43%, 1)',
+        borderColor: 'hsla(215, 57%, 60%, 1)',
+        // TODO set initial data as empty to make a good range
+        data
+      }]
+    },
+    options: {
+      legend: {
+        display: false
+      },
+      scales: {
+        xAxes: [{
+          type: 'time',
+          distribution: 'linear',
+          time: {
+            unit: 'day',
+            min: new Date(lower),
+            max: new Date(upper),
+            tooltipFormat: 'MMMM D',
+            stepSize: 7
+          },
+          bounds: 'ticks',
+          ticks: {
+            // maxTicksLimit: 4
+          },
+          gridLines: {
+            display: false
+          },
+          maxBarThickness: 20
+        }],
+
+        yAxes: [{
+          ticks: {
+            min: 0,
+            max: Math.max(localMax, 10),
+            stepSize: 5
+          }
+        }]
+      },
+      animation: {
+          // duration: 300
+      }
+    }
+  }
 }
