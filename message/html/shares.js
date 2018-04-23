9237b9df2489619d235754cd0b215e38bc426234
andre alves garzia <andre@andregarzia.com>
2018-04-23 18:33:33 -0300

    Added lightbox for captioning

--- message/html/shares.js
+++ message/html/shares.js
@@ -1,10 +1,12 @@
-var { h, computed, when } = require('mutant')
+var { h, computed, when, Value, resolve } = require('mutant')
 var nest = require('depnest')
 
 exports.needs = nest({
   'keys.sync.id': 'first',
   'message.obs.shares': 'first',
-  'sbot.async.publish': 'first'
+  'sbot.async.publish': 'first',
+  'translations.sync.strings': 'first',
+  'app.html.lightbox': 'first'
 })
 
 exports.gives = nest('message.html.shares')
@@ -16,17 +18,51 @@ exports.create = (api) => {
 
     var iShared = computed(shares, shares => shares.includes(id))
     var count = computed(shares, shares => shares.length ? shares.length : '')
-    return h('Shares', { 'ev-click': () => publishShare(msg, !iShared()) }, [
+    var isOpen = Value(false)
+    var strings = api.translations.sync.strings()
+    var publishAndClose = (msg) => {
+      publishShare(msg, resolve(captionRaw))
+      isOpen.set(false)
+    }
+
+    var captionRaw = Value('')
+    var captionInput = h('textarea#caption', {
+      style: {
+        width: '90%'
+      },
+      placeholder: 'Type an optional caption here',
+      value: computed(captionRaw, t => t),
+      'ev-input': () => captionRaw.set(captionInput.value),
+    })
+
+    var confirmationDialog = h('div.dialog', [
+      h('div.message', [
+        h('p', 'Do you to share this post with your followers?'),
+      ]),
+      h('div.form', [
+        captionInput
+      ]),
+      h('div.actions', [
+        h('Button', { 'ev-click': () => isOpen.set(false) }, strings.userShow.action.cancel),
+        h('Button -primary', { 'ev-click': () => publishAndClose(msg, true) }, "Share")
+      ])
+    ])
+
+    var lb = api.app.html.lightbox(confirmationDialog, isOpen)
+
+
+    return h('Shares', { 'ev-click': () => isOpen.set(!iShared()) }, [
       h('i.fa', { className: when(iShared, 'fa-retweet', 'fa-retweet faint') }),
-      h('div.count', count)
+      h('div.count', count),
+      lb
     ])
   })
 
-  function publishShare(msg, status = true) {
+  function publishShare(msg, text, status = true) {
     if (status) {
       var share = {
         type: 'share',
-        share: { link: msg.key, content: "blog", text: '' }
+        share: { link: msg.key, content: "blog", text: text }
       }
       if (msg.value.content.recps) {
         share.recps = msg.value.content.recps.map(function (e) {
