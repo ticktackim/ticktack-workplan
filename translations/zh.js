30548b332f1b816d67ed2667c88ba6d6d3269903
mix irving <whimful@gmail.com>
2018-06-01 15:21:17 +1200

    update translations

--- translations/zh.js
+++ translations/zh.js
@@ -1,147 +1,226 @@
 module.exports = {
-  loading: '加载中...',
-  optionalField: '可选',
-  sendMessage: '发送',
-  showMore: '显示更多',
-  channel: '频道',
-  directMessages: '私信',
-  home: '主页',
-  error: '错误',
-  errorNotFound: '找不到此页面',
-  groupNew: '新建群组',
-  writeMessage: '发私信',
-  writeComment: '留言',
-  peopleNearby: '附件的朋友',
-  addressBook: {
-    heading: {
-      people: '用户'
+  'loading': '加载中...',
+  'optionalField': '可选',
+  'sendMessage': '发送',
+  'showMore': '显示更多',
+  'channel': '频道',
+  'directMessages': '私信',
+  'home': '主页',
+  'error': '错误',
+  'errorNotFound': '找不到此页面',
+  'groupNew': '新建群组',
+  'writeMessage': '发私信',
+  'writeComment': '留言',
+  'peopleNearby': '附件的朋友',
+  'addressBook': {
+    'heading': {
+      'people': '用户'
     },
-    section: {
-      friends: '好友',
-      following: '我关注的',
-      followers: '关注我的'
+    'section': {
+      'friends': '好友',
+      'following': '我关注的',
+      'followers': '关注我的'
     },
-    action: {
-      addUser: '添加好友',
-      find: {
-        friends: '查找好友',
-        following: '查找我关注的人',
-        followers: '查找关注我的人',
-        search: '查找用户'
+    'action': {
+      'addUser': '添加好友',
+      'find': {
+        'friends': '查找好友',
+        'following': '查找我关注的人',
+        'followers': '查找关注我的人',
+        'search': '查找用户'
       }
     }
   },
-  blogIndex: {
-    title: '发现'
+  'blogIndex': {
+    'title': '发现'
   },
-  blogNew: {
-    field: {
-      title: '标题',
-      summary: '序言'
+  'blogNew': {
+    'field': {
+      'title': '标题',
+      'summary': '序言'
     },
-    actions: {
-      edit: '编辑',
-      preview: '预览',
-      writeBlog: '撰文'
+    'actions': {
+      'edit': '编辑',
+      'preview': '预览',
+      'writeBlog': '撰文'
     }
   },
-  channelShow: {
-    action: {
-      subscribe: '订阅',
-      unsubscribe: '取消订阅'
+  'channelShow': {
+    'action': {
+      'subscribe': '订阅',
+      'unsubscribe': '取消订阅'
     }
   },
-  topNav: {
-    blogsAll: '最新',
-    blogSearch: '搜索'
+  'topNav': {
+    'blogsAll': '最新',
+    'blogSearch': '搜索'
   },
-  groupFind: {
-    action: {
-      findAGroup: '查找群组',
-      newGroup: '创建此群组'
+  'groupFind': {
+    'action': {
+      'findAGroup': '查找群组',
+      'newGroup': '创建此群组'
     },
-    state: {
-      groupNotFound: '此群组未被创建'
+    'state': {
+      'groupNotFound': '此群组未被创建'
     },
-    flash: {
-      createFirstThread: '发布第一个话题来开始群组讨论'
+    'flash': {
+      'createFirstThread': '发布第一个话题来开始群组讨论'
     }
   },
-  groupIndex: '群组列表',
-  settingsPage: {
-    title: '设置',
-    action: {
-      edit: '编辑'
+  'groupIndex': '群组列表',
+  'settingsPage': {
+    'title': '设置',
+    'action': {
+      'edit': '编辑'
     },
-    section: {
-      language: '语言',
-      profile: '自我介绍',
-      name: '名称',
-      introduction: '简介',
-      zoom: '放大'
+    'section': {
+      'language': '语言',
+      'profile': '自我介绍',
+      'name': '名称',
+      'introduction': '简介',
+      'zoom': '放大',
+      'version': '版本',
+      'theme': '背景'
     }
   },
-  threadNew: {
-    pageTitle: '新建话题',
-    field: {
-      to: '发送给',
-      subject: '主题'
+  'threadNew': {
+    'pageTitle': '新建话题',
+    'field': {
+      'to': '发送给',
+      'subject': '主题'
     },
-    action: {
-      new: '创建私信',
-      addMoreRecps: '添加多个用户（可选）'
+    'action': {
+      'new': '创建私信',
+      'addMoreRecps': '添加多个用户（可选）'
     }
   },
-  threadShow: '所有私信',
-  userEdit: {
-    section: {
-      name: '名称',
-      avatar: '头像',
-      introduction: '简介'
+  'threadShow': '所有私信',
+  'userEdit': {
+    'section': {
+      'name': '名称',
+      'avatar': '头像',
+      'introduction': '简介'
     },
-    instruction: {
-      crop: '点击编辑头像'
+    'instruction': {
+      'crop': '点击编辑头像'
     },
-    action: {
-      okay: '完成',
-      cancel: '取消',
-      save: '保存'
+    'action': {
+      'okay': '完成',
+      'cancel': '取消',
+      'save': '保存'
     }
   },
-  userFind: {
-    action: {
-      findAUser: '查找用户'
+  'userFind': {
+    'action': {
+      'findAUser': '查找用户'
     }
   },
-  userShow: {
-    action: {
-      follow: '关注',
-      unfollow: '取消关注',
-      directMessage: '创建私信',
-      block: '拉黑',
-      unblock: '原谅',
-      blockConfirmationMessage: '拉黑意味着您再也不会接收到此用户的信息',
-      cancel: '取消'
+  'userShow': {
+    'action': {
+      'follow': '关注',
+      'unfollow': '取消关注',
+      'directMessage': '创建私信',
+      'block': '拉黑',
+      'unblock': '原谅',
+      'blockConfirmationMessage': '拉黑意味着您再也不会接收到此用户的信息',
+      'cancel': '取消'
     },
-    state: {
-      friends: '你们成为了好友',
-      youFollow: '你关注了的',
-      theyFollow: '关注了你的',
-      userIsInGroups: '所在的群组：',
-      userConversationsWith: '参与的话题',
-      follow: '关注',
-      friendsInCommon: '共同的好友'
+    'state': {
+      'friends': '你们成为了好友',
+      'youFollow': '你关注了的',
+      'theyFollow': '关注了你的',
+      'userIsInGroups': '所在的群组：',
+      'userConversationsWith': '参与的话题',
+      'follow': '关注',
+      'friendsInCommon': '共同的好友'
     }
   },
-  subscriptions: {
-    user: '我的订阅',
-    friends: '朋友的订阅',
-    state: {
-      noSubscriptions: '您还没有任何订阅'
+  'subscriptions': {
+    'user': '我的订阅',
+    'friends': '朋友的订阅',
+    'state': {
+      'noSubscriptions': '您还没有任何订阅'
     }
   },
-  languages: {
-    en: 'English',
-    zh: '中文'
+  'languages': {
+    'en': 'English',
+    'zh': '中文'
+  },
+  'stats': '统计 ',
+  'comments': '留言',
+  'likes': '点赞',
+  'shares': '分享',
+  'themes': {
+    'light': '浅色背景',
+    'dark': '深色背景'
+  },
+  'splash': {
+    'about': [
+      '开放、平等、自由是我们的核心价值观',
+      '',
+      '致力于打造一个连接数十亿用户的开放、自由、平等的去中心化社交平台',
+      '赋能用户进行知识分享与价值交换。'
+    ]
+  },
+  'pluginsOk': {
+    'action': {
+      'ok': 'Okay'
+    }
+  },
+  'publishBlog': '发表',
+  'share': {
+    'captionPlaceholder': '（可选）添加说明',
+    'shareLabel': '向您的关注者分享此文章',
+    'externalShareLabel': '分享此文章到外部网页',
+    'action': {
+      'share': '分享',
+      'copy': '复制链接',
+      'open': '打开链接'
+    },
+    'settings': {
+      'caption': '统计隐私设置',
+      'publicOption': '全部公开',
+      'authorAndYouOption': '仅向作者与自己公开',
+      'justYouOption': '仅向自己公开'
+    }
+  },
+  'statsShow': {
+    'title': '统计',
+    'prevMonth': '前30天',
+    'nextMonth': '后30天',
+    'thirtyDays': '30天'
+  },
+  'backup': {
+    'sectionName': '备份',
+    'ftu': {
+      'importAction': '导入身份私钥文件',
+      'createAction': '创建新的身份文件',
+      'busyMessage': '处理中......',
+      'welcomeMessage': '您希望创建新的身份文件还是导入之前的备份文件？'
+    },
+    'export': {
+      'header': '导出身份私钥文件',
+      'message': {
+        '请谨慎备份并妥善保存好您的身份私钥文件，平台不会备份及保存您的个人资料。': true,
+        '一旦您的身份私钥文件被盗取，您的个人身份将有可能被第三方冒充，您的所有加密信息也会被第三方读取。': true
+      },
+      'passwordPlaceholder': '请输入密码以防止私钥被恶意导出。请务必记住并保管好密码，平台不会保存您的任何个人密码。',
+      'cancelAction': '取消',
+      'exportAction': '导出身份私钥文件',
+      'dialog': {
+        'label': '导出身份私钥文件',
+        'title': '导出身份私钥文件'
+      }
+    },
+    'import': {
+      'importAction': '导入身份私钥文件',
+      'header': '导入身份私钥文件',
+      'synchronizeMessage': '处理中......',
+      'dialog': {
+        'label': '导入身份私钥文件',
+        'title': '导入身份私钥文件'
+      },
+      'details': '重建您的身份需要一些时间。一旦您的身份与网络同步，Ticktack将正常启动，并需要一些时间来更新您的朋友圈数据，所以一些信息和博文将在稍后送达。您可以安全地关闭窗口退出程序，下次打开Ticktack时，Ticktack将恢复同步信息。'
+    }
   }
 }
