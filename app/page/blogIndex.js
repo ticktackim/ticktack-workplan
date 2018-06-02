5831f345af34aebcc413b1b452dadcbf6244b419
andre alves garzia <andre@andregarzia.com>
2018-06-02 00:11:52 -0300

    implemented filters for dynamics

--- app/page/blogIndex.js
+++ app/page/blogIndex.js
@@ -1,5 +1,5 @@
 const nest = require('depnest')
-const { h, Array: MutantArray, resolve } = require('mutant')
+const { h, Array: MutantArray, resolve, when, computed } = require('mutant')
 const Scroller = require('mutant-scroll')
 const pull = require('pull-stream')
 
@@ -18,6 +18,8 @@ exports.needs = nest({
   'sbot.obs.connection': 'first',
   'history.sync.push': 'first',
   'keys.sync.id': 'first',
+  'contact.obs.followers': 'first',
+  'channel.obs.isSubscribedTo': 'first',
   'message.sync.isBlocked': 'first',
   'translations.sync.strings': 'first',
   'unread.sync.isUnread': 'first'
@@ -30,25 +32,28 @@ exports.create = (api) => {
     // location here can expected to be: { page: 'blogIndex'}
 
     var strings = api.translations.sync.strings()
+    blogsCache.clear()
 
     var blogs = Scroller({
       classList: ['content'],
       prepend: api.app.html.topNav(location),
-      streamToTop: Source({ reverse: false, live: true, old: false, limit: 20 }),
-      streamToBottom: Source({ reverse: true, live: false, limit: 20 }),
+      streamToTop: Source({ reverse: false, live: true, old: false, limit: 20 }, api, location),
+      streamToBottom: Source({ reverse: true, live: false, limit: 20 }, api, location),
       updateTop: update,
       updateBottom: update,
       store: blogsCache,
       render
     })
 
-    return h('Page -blogIndex', {title: strings.home}, [
+
+
+    return h('Page -blogIndex', { title: strings.home }, [
       api.app.html.sideNav(location),
       blogs
     ])
   })
 
-  function Source (opts) {
+  function Source(opts, api, location) {
     const commonOpts = {
       query: [{
         $filter: {
@@ -62,18 +67,48 @@ exports.create = (api) => {
       }]
     }
 
+    const myId = api.keys.sync.id()
+    const { followers } = api.contact.obs
+    const activeFilter = location.hasOwnProperty('filter') ? location.filter : "All"
+
+    const filterbyFriends = (msg) => {
+      if (activeFilter !== 'Friends') return true
+
+      const feed = msg.value.author
+      const theirFollowers = followers(feed)
+      const youFollowThem = computed(theirFollowers, followers => followers.includes(myId))
+      const r = resolve(youFollowThem)
+
+      return r
+    }
+
+    const filterBySubscription = (msg) => {
+      if (activeFilter !== 'Subscriptions') return true
+
+      if (!msg.value.content.hasOwnProperty('channel')) {
+        return false
+      }
+
+      const isSubscribedTo = api.channel.obs.isSubscribedTo
+      const channel = msg.value.content.channel
+
+      return resolve(api.channel.obs.isSubscribedTo(channel))
+    }
+
     return pull(
       StepperStream(
         (options) => api.sbot.pull.stream(sbot => sbot.query.read(options)),
         Object.assign(commonOpts, opts)
       ),
       pull.filter(api.blog.sync.isBlog), // isBlog or Plog?
+      pull.filter(filterBySubscription),
+      pull.filter(filterbyFriends),
       // pull.filter(msg => !msg.value.content.root), // show only root messages
       pull.filter(msg => !api.message.sync.isBlocked(msg)) // this is already in feed.pull.type
     )
   }
 
-  function update (soFar, newBlog) {
+  function update(soFar, newBlog) {
     soFar.transaction(() => {
       var object = newBlog // Value(newBlog)
 
@@ -91,7 +126,7 @@ exports.create = (api) => {
     })
   }
 
-  function render (blog) {
+  function render(blog) {
     const { recps, channel } = blog.value.content
     var onClick
     if (channel && !recps) { onClick = (ev) => api.history.sync.push(Object.assign({}, blog, { page: 'blogShow' })) }
@@ -99,7 +134,7 @@ exports.create = (api) => {
   }
 }
 
-function indexOf (array, fn) {
+function indexOf(array, fn) {
   for (var i = 0; i < array.getLength(); i++) {
     if (fn(array.get(i))) {
       return i
@@ -110,7 +145,7 @@ function indexOf (array, fn) {
 
 // this is needed because muxrpc doesn't do back-pressure yet
 // this is a modified pull-next-step for ssb-query
-function StepperStream (createStream, _opts) {
+function StepperStream(createStream, _opts) {
   var opts = clone(_opts)
   var last = null
   var count = -1
@@ -142,7 +177,7 @@ function StepperStream (createStream, _opts) {
           }
         },
         (err) => {
-            // retry on errors...
+          // retry on errors...
           if (err) {
             count = -1
             return count
